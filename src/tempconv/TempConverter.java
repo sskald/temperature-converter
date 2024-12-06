0456d7f72873889e4995486f60ab03d1a0fcf88f
timofey <timofejsuslov437@gmail.com>
2024-12-06 20:24:08 +0300

    Added temperature input method

--- src/tempconv/TempConverter.java
+++ src/tempconv/TempConverter.java
@@ -7,8 +7,21 @@ Scanner sc = new Scanner(System.in);
 		double t = getTemp();
 		char si1 = getSI();
 		char si2 = getSI();
-		double result = calc(double t, char si1, char si2);
+		double result = calc(t, si1, si2);
 		System.out.println("Перевод выполнен: " + result);
 	}
-
+	
+	public static double getTemp() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Введите t*: ");
+		if(sc.hasNextDouble()) {
+			return(sc.nextDouble());
+		} else {
+			System.out.println("ОШИБКА ПРИ ВВОДЕ. ПОВТОРИТЕ ПОПЫТКУ");
+			return getTemp();
+		}
+	}
+	public static char getSI() {
+		
+	}
 }
